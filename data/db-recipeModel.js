b22504304f8a410b0d1f684dec32acd66b08f349
Matthew Bolton <matthewwbolton@gmail.com>
2020-04-16 19:05:14 -0400

    created express router file for all endpoints associated with the 'recipe' resource

--- data/db-recipeModel.js
+++ data/db-recipeModel.js
@@ -12,6 +12,16 @@ function getRecipes() {
   return db("recipes");
 }
 
-function getShoppingList(recipe_id) {}
+function getShoppingList(recipe_id) {
+  db.select("i.ingredient_name", "ri.quantity")
+    .from("ingredients as i")
+    .join("recipe_ingredients as ri", "i.id", "ri.ingredient_id")
+    .where("ri.recipe_id", recipe_id);
+}
 
-function getInstructions(recipe_id) {}
+function getInstructions(recipe_id) {
+  db.select("instruction")
+    .from("recipes as r")
+    .join("instructions as i", "r.id", "i.recipe_id")
+    .where("r.id", recipe_id);
+}
--- recipes/RecipeRouter.js
+++ recipes/RecipeRouter.js
@@ -16,4 +16,43 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id/ingredients", (req, res) => {
+  const { id } = req.params;
+  Recipes.getShoppingList(id)
+    .then((ingredients) => {
+      if (ingredients.length) {
+        res.status(200).json(ingredients);
+      } else {
+        res
+          .status(404)
+          .json({
+            error: `The recipe with an ID of ${id} has no ingredients associated with it`,
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: `The recipe with the id of ${id} cannot be found.` });
+    });
+});
+
+router.get("/:id/instructions", (req, res) => {
+  const { id } = req.params;
+  Recipes.getInstructions(id)
+    .then((instructions) => {
+      if (instructions.length) {
+        res.status(200).json(instructions);
+      } else {
+        res.status(500).json({
+          error: `There were no instructions found for the recipe with ID ${id}`,
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "server error" });
+    });
+});
+
 module.exports = router;
